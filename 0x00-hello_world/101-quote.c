312ed7fa9cb91f772bee42d6133a605791d3768a
Oluwaseyi Oyewole <sheyiphill@gmail.com>
2022-03-10 08:32:09 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  main - Prints out the least part of a quoute in the standard error.
+ *  Return: 1 if success.
+ */
+int main (void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
