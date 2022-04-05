ebe39b7daab2966a68a9c8e6033e5a9e0923dcbf
Oluwaseyi Oyewole <sheyiphill@gmail.com>
2022-04-05 14:26:15 +0000

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -49,15 +49,15 @@ char **strtow(char *str)
 			if (str[c] == ' ')
 				a1++;
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			{	
+			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
-				{	
+				{
 					ch_free_grid(aout, i);
 					return (NULL);
-				}	
+				}
 				break;
-			}	
+			}
 		}
 		for (j = 0; a1 <= c; a1++, j++)
 			aout[i][j] = str[a1];
