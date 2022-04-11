ca5b47b7a4034720151220f65d1b8b6d030e2dee
Oluwaseyi Oyewole <sheyiphill@gmail.com>
2022-04-11 15:59:32 +0000

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 3-function_like_macro.h
- * 
+ * Auth: Oluwaseyi 
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
